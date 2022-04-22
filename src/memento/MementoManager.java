456bab6ebd845cfe13f605b4bcb0a733f7727535
MatthieuDuretOsys <72560512+MatthieuDuretOsys@users.noreply.github.com>
2022-04-22 09:43:43 +0200

    Memento

---
+++ src/memento/MementoManager.java
@@ -0,0 +1,58 @@
+package memento;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import model.Message;
+
+
+
+public class MementoManager {
+
+	private Map<Integer, List<MementoMessage>> MementoMessages = new HashMap<>();
+	private int currentMessageIndex = -1;
+
+	public void ajouterMemento(Message message) {
+		if (!this.MementoMessages.containsKey(message.getEmetteur().getId())) {
+			this.MementoMessages.put(message.getEmetteur().getId(), new ArrayList<>());
+		}
+
+		List<MementoMessage> mementos = this.MementoMessages.get(message.getEmetteur().getId());
+
+		while (mementos.size() - 1 > currentMessageIndex) {
+			mementos.remove(mementos.size() - 1);
+		}
+
+		mementos.add(new MementoMessage(message));
+		currentMessageIndex++;
+
+		if (mementos.size() > 5) {
+			mementos.remove(0);
+		}
+	}
+
+	public void mementoPrecedent(Message Message) {
+		if (this.MementoMessages.containsKey(Message.getEmetteur().getId())) {
+			List<MementoMessage> mementos = this.MementoMessages.get(Message.getEmetteur().getId());
+			
+			System.out.println("PREVIOUS");
+			mementos.get(--currentMessageIndex).restore(Message);
+		}
+	}
+
+	public void mementoSuivant(Message Message) {
+		if (this.MementoMessages.containsKey(Message.getEmetteur().getId())) {
+			List<MementoMessage> mementos = this.MementoMessages.get(Message.getEmetteur().getId());
+
+			if (mementos.size() - 2 >= currentMessageIndex) {
+				System.out.println("NEXT");
+				mementos.get(++currentMessageIndex).restore(Message);
+			}
+		}
+	}
+
+	
+	
+}
