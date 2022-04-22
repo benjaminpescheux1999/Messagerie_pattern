44e8890a12b9b0aa9d06189231b49aff3c343e62
benjaminpescheux1999 <71870893+BenjaminITTF@users.noreply.github.com>
2022-04-22 10:05:42 +0200

    Update Application.java

--- src/messenger/Application.java
+++ src/messenger/Application.java
@@ -13,32 +13,38 @@ import state.PresentState;
 public class Application {
 	public static void main(String... args) {
 		
-		//Singleton context
+		//Singleton context (try catch)
 		DbContext db = DbContext.getInstance();
-		System.out.println(db);
-
+		
+		//Connexion
+		
+		//state (Si connecté change state en présent et passer au proxy)
+				Context context = new Context();
+				 
+				PresentState presentState = new PresentState();
+				presentState.doAction(context);
+				System.out.println(context.getState().toString());
+				 
+				AbsentState absentState = new AbsentState();
+				absentState.doAction(context);
+				 
+				System.out.println(context.getState().toString());
+		//state sinon break
+				
 		//Proxy
 		IMessageService srvMessage = new MessageService();
-
 		// On rÃ©cupÃ¨re l'instance de la liste, sans la manipuler pour le moment
 		List<Message> messages = srvMessage.findAll();
 		System.out.println("PAS ENCORE UTILISEE, DONC PAS DE CHARGEMENT ICI");
 
 		// On manipule la liste en demandant sa taille, le chargement se fera Ã  ce moment
-		System.out.println(messages.size());
+		System.out.println(""+messages.size());
 		System.out.println("UTILISEE, DONC CHARGEMENT !");
 		
-		//state
-		Context context = new Context();
-		 
-		PresentState presentState = new PresentState();
-		presentState.doAction(context);
-		System.out.println(context.getState().toString());
-		 
-		AbsentState absentState = new AbsentState();
-		absentState.doAction(context);
-		 
-		System.out.println(context.getState().toString());
+		
+		
+		
+		
 
 		
 	}
